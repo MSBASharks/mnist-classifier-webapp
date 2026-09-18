369124ebb5412371110531f9d6df6122238b2e24
ABrooks <91301993+Andrewjbro@users.noreply.github.com>
2026-09-17 21:21:34 -0500

    Reduce Gunicorn workers to lower memory usage

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,4 +13,4 @@ echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
 echo "Starting gunicorn on 0.0.0.0:8000..."
-exec gunicorn mysite.wsgi:application --bind 0.0.0.0:8000 --workers 2
+exec gunicorn mysite.wsgi:application --bind 0.0.0.0:8000 --workers 1
